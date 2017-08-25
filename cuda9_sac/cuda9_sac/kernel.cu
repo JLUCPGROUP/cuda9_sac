2f7ec246a67fa9039510c9b55c1771b58d3d533c
李哲 <leezear@live.cn>
2017-08-25 21:30:18 +0800

    添加所有代码

---
+++ cuda9_sac/cuda9_sac/kernel.cu
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "cudasac.cuh"
+#undef DOMDocument
+
+#include "XBuilder.h"
+
+using namespace std;
+using namespace cudacp;
+const string X_PATH = "BMPath.xml";
+
+int main()
+{
+	XBuilder path_builder(X_PATH, XRT_BM_PATH);
+	string bm_path = path_builder.GetBMFile();
+	cout << bm_path << endl;
+	XBuilder builder(bm_path, XRT_BM);
+	XModel* xmodel = new XModel();
+	builder.GenerateModelFromXml(xmodel);
+	float build_time = BuidBitModel32bit(xmodel);
+	float exe_time = SACGPU();
+	DelGPUModel();
+
+	delete xmodel;
+	xmodel = NULL;
+	printf("Build time = %f\n", build_time);
+	printf("Execution time = %f\n", exe_time);
+	printf("---end---\n");
+	return 0;
+}
+
